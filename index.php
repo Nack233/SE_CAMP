c07a2879b2e52c2bf4854051a0310ab24f549af1
Nack233 <65160233@go.buu.ac.th>
2023-12-25 18:27:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
+  <meta charset="utf-8">
 </head>
 
 <body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+  <?php $multi_x = 2; ?>
+  <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
 
+  <div class="box">
     <table>
-        <tbody>
-            <tr>
-            <?php
-    $b = 2;
-    for( $i=1; $i<=24; $i++ ) {
-        echo "{$b} x {$i} = ".($b*$i)."<br/>";
-    }
-?>
-            </tr>
-        </tbody>
+      <tbody>
+        <tr>
+          <?php
+          $b = 2;
+          for ($i = 1; $i <= 24; $i++) {
+            echo "{$b} x {$i} = " . ($b * $i) . "<br/>";
+          }
+          ?>
+        </tr>
+      </tbody>
     </table>
-    <style>
-        table {
-            width: 150px;
-            height: 265px;
-            overflow: hidden;
-        }
-        tbody{
-            overflow: hidden;
-        }
-    </style>
+  </div>
+
+  <style>
+    .box {
+      width: 150px;
+      height: 265px;
+      overflow: auto;
+    }
+  </style>
 </body>
 
 </html>
